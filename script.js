51fe856f81c33b823a7675aace241d65f53896f0
pulakk-sasmal <94732152+pulakk-sasmal@users.noreply.github.com>
2023-11-26 21:10:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// // Add JavaScript for the login animation, logout functionality, and other dynamic behaviors here
+// document.getElementById('loginButton').addEventListener('click', () => {
+//     // Implement your login animation here
+//     // After successful login, redirect to home.html
+//     window.location.href = 'home.html';
+// });
+
+// document.getElementById('logoutButton').addEventListener('click', () => {
+//     // Implement your logout functionality here
+//     // After logout, redirect back to index.html
+//     window.location.href = 'index.html';
+// });
